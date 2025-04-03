866f45371fefe4272285e9df1b24c6a246159ef5
Victor Malchikov <victormalchikov87@gmail.com>
2025-04-03 11:08:04 -0700

    static header content

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Victor Malchikov",
@@ -10,5 +11,28 @@ export default function ArchiveLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="min-h-screen">{children}</div>;
+  return (
+    <div className="min-h-screen scroll-smooth">
+      <h1>Victor Malchikov</h1>
+      <h2>Software Developer</h2>
+      <p>I work in the cloud and build applications for the web!</p>
+
+      {/* Internal navigation using Next.js Link */}
+      <nav>
+        <ul className="space-y-2">
+          <li>
+            <Link href="#about">About</Link>
+          </li>
+          <li>
+            <Link href="#experience">Experience</Link>
+          </li>
+          <li>
+            <Link href="#projects">Projects</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <main>{children}</main>
+    </div>
+  );
 }
