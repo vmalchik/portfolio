5cc54b27f39e090f994188fa352707c124898902
Victor Malchikov <victormalchikov87@gmail.com>
2025-04-04 01:09:26 -0700

    wip about me

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,66 +1,37 @@
 export default function HomePage() {
+  // https://www.complytec.com/wp-content/uploads/RSA-Archer-Gartner-Reports.pdf
+  // https://www.archerirm.community/s/blogs/rsa-named-a-leader-in-2020-gartner-magic-quadrant-for-it-vendor-risk-management-MCWZPJKWOQVZE5ZA3ZYQMWGHIKQU
+  // https://www.archerirm.com/archer-engage-for-vendors
   return (
     <main>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
+      <p>
+        I&apos;m a full-stack software engineer with a focus on cloud
+        architecture and a passion for front-end development. I enjoy working
+        across the stack — from building UI in React to designing scalable
+        systems in AWS. I&apos;m most at home when prototyping, collaborating,
+        and diving deep into new ideas. I&apos;m drawn to projects where I can
+        contribute to how things are designed — not just how they&apos;re
+        implemented.
+      </p>
+      <p>
+        As a Senior Principal Developer at RSA, I led I led front-end and cloud
+        development across multiple business units, including NetWitness and
+        Archer. I helped launch a SaaS risk platform that contributed to
+        RSA&apos;s recognition in Gartner&apos;s Magic Quadrant and filed
+        several patents around risk modeling and access policy automation.
+      </p>
+      <p>
+        Before that, I was a founding engineer at Genify, where I built a
+        component-driven front-end architecture that scaled from MVP to
+        acquisition. I've contributed to early-stage startups, enterprise
+        platforms, and developer tooling — most recently launching WordMuse AI,
+        a serverless app that helps users write thoughtful messages using LLMs.
+      </p>
+      <p>
+        When I’m not building, I enjoy hiking and volunteering (most recently
+        with Second Harvest). I live in San Jose with two birds — an African
+        Grey and a parakeet — who mostly supervise from the sidelines.
+      </p>
     </main>
   );
 }
