c37d9a1b23fccdefc44eaada69d6f4e54c378fc9
Victor Malchikov <victormalchikov87@gmail.com>
2025-04-04 14:49:26 -0700

    stub experience card

--- src/components/page/home/experience-section.tsx
+++ src/components/page/home/experience-section.tsx
@@ -6,7 +6,37 @@ export const ExperienceSection = ({ id }: SectionProps) => {
     <section id={id} className="space-y-6">
       <SectionHeader>Experience</SectionHeader>
 
-      <div></div>
+      <div>
+        {/* description - can be text, image, text + image */}
+        {/* should be provided as separate props */}
+        <span>2024 - Present</span>
+        <span>Some Image</span>
+        {/* title */}
+        <span>Senior Principal Software Engineer</span>
+        {/* optional sub-title */}
+        <span>Principal Software Engineer</span>
+        <p>
+          As a Senior Principal Engineer at RSA, I led front-end and cloud
+          development across multiple business units, including NetWitness,
+          Archer and SecurID. I helped launch a SaaS risk platform that
+          contributed to RSA's leader recognition in Gartner's Magic Quadrant
+          and filed several patents around risk modeling and access policy
+          automation.
+        </p>
+        <div>
+          {/* each element here should be it's own component */}
+          <span>Project 1</span>
+          <span>Project 2</span>
+          <span>Project 3</span>
+        </div>
+        <div>
+          {/* each element here should be it's own component */}
+          <span>JavaScript</span>
+          <span>TypeScript</span>
+          <span>AWS</span>
+          <span>AWS</span>
+        </div>
+      </div>
     </section>
   );
 };
