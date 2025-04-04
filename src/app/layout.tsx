a72a9e61a1398d8fbef6145a60cfd232e21d4aa0
Victor Malchikov <victormalchikov87@gmail.com>
2025-04-04 00:06:57 -0700

    global layout styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "Victor Malchikov",
@@ -13,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${inter.className} bg-slate-900 leading-relaxed antialiased text-slate-400 selection:bg-teal-300 selection:text-teal-900`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
