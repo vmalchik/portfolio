5eb0e489a9ed96491e3f8e79a4819bcf7cf0262e
Victor Malchikov <victormalchikov87@gmail.com>
2025-04-06 00:14:12 -0700

    spotlight comments

--- src/components/common/global-spotlight.tsx
+++ src/components/common/global-spotlight.tsx
@@ -2,6 +2,13 @@
 
 import { useEffect, useRef } from "react";
 
+// This component creates a spotlight effect on the screen that follows the mouse cursor.
+// It uses a radial gradient to create the spotlight effect and adjusts its position based on mouse movement.
+// The spotlight effect is achieved by applying a radial gradient background to a fixed div that covers the entire screen.
+// The opacity of the spotlight is animated to create a smooth transition effect.
+// The component uses the useRef and useEffect hooks from React to manage the spotlight's position and opacity.
+// Adopted from https://ibelick.com/blog/create-modern-spotlight-effect-with-react-css
+
 export const GlobalSpotlight = () => {
   const spotlightRef = useRef<HTMLDivElement>(null);
 
